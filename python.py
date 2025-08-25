4983e24348a2ae1cb1de4c7111534859a577894b
Elsamariatomlin <120251422+Elsamariatomlin@users.noreply.github.com>
2025-08-25 13:08:27 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,17 +1,33 @@
-class vehicle:
-    def __init__(self,name,color,price):
+# class Animal:
+#     def speak(self):
+#         print("Animal speaks")
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Dog barks")  # Overrides parent method
+
+# Dog().speak()  # Output: Dog barks
+
+
+
+class Fruits:
+    def taste(self):
+        print("fruits are sweet")
+    def __init__(self,name,color,shape):
         self.name=name
         self.color=color
-        self.price=price
+        self.shape=shape
     def details(self):
-        print(f"name:{self.name},color:{self.color},price:{self.price}")
-
-class car(vehicle):
-    def __init__(self, name, color, price,speed):
-        super().__init__(name, color, price)
-        self.speed=speed
-    def car_details(self):
-            super().details()
-            print(f"speed:{self.speed}")
-car1=car("xuv","blue",249000,120)
-car1.car_details()
+          print(f"the name of the fruit is {self.name} and the color is {self.color} and the shape is {self.shape}")
+class mango(Fruits):
+    def __init__(self,name,color,shape,price):
+        super().__init__(name,color,shape)
+        self.price=price
+    def display(self):
+            print(f"the price of the mango is {self.price}")
+f1=mango("mango","green","round",100)
+f1.details()
+f1.display()
+f2=mango("mango","yellow","round",130)
+f2.details()
+f2.display()
